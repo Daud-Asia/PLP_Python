2ea8673aece25c2553c468a0dd9a8b107f58a564
Daud Asia <atinukeasia@gmail.com>
2025-08-10 18:02:09 +0100

    Update week2 assignment.py

--- week2 assignment.py
+++ week2 assignment.py
@@ -1,1 +1,23 @@
+1.
+my_list = []
+
+2. 
+my_list.extend([10, 20, 30, 40])
+
+3. 
+my_list.insert(1, 15)
+
+4.
+my_list.extend([50, 60, 70])
+
+5.
+my_list.pop()
+
+6.
+my_list.sort()
+
+7. 
+my_list.index(30)
+print("Index of 30:", index_30)
+
 
